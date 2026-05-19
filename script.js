cf8ad842cc4c47926845e1be099a29a77f6e990f
thosea273 <tiffanyhosea180@gmail.com>
2026-05-20 01:07:32 +1000

    js open cart popup and go to product page

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ function openCartDrawer() {
 
 }
 
+/*product*/
+let currentPage = "p"; /*masuk memori lg di hlmn mana*/
+  function goto(page) {
+    document.querySelectorAll(".page").forEach(p => {
+      p.classList.remove("active"); /*hide*/
+    });
+  const target = document.getElementById("page-" + page); /*cari di main*/
+
+    if (!target) { console.warn("Page not found:", page); return; }
+
+  target.classList.add("active");
+    currentPage = page;
+    window.scrollTo({ top: 0, behavior: "instant" });
+  
+
+
+}
