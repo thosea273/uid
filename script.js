0a2145c99a2563a88a074b0d50b35da07396270b
thosea273 <tiffanyhosea180@gmail.com>
2026-05-21 17:48:33 +1000

    products js

--- script.js
+++ script.js
@@ -16,7 +16,63 @@ let currentPage = "p"; /*masuk memori lg di hlmn mana*/
   target.classList.add("active");
     currentPage = page;
     window.scrollTo({ top: 0, behavior: "instant" });
-  
 
 
 }
+
+/*data*/
+
+const PRODUCTS= [
+{
+    id: 2, 
+    name: "HOUND COMPRESSION",
+    price: 64,
+    currency: "AUD",
+    color: "#232325",
+    img: "two.pg"
+    detail: {
+        badge:"PRE-ORDER",
+        title: "HOUND COMPRESSION",
+        desc: `The most requested relic in the archive answers the call. The Onyx returns with a sigil chain marking the left arm. Those who waited since Black Friday finally claim what was promised.`,
+        features: [
+        "HOUND 3M Reflective Heart Logo",
+        "HOUND 3M Reflective Back Logo",
+        "HOUND 3M Reflective Sleeve Submark Label",
+        "HOUND ContourFit Seam Pattern",
+        "180GSM NylonSpandex Blend",
+        "Model is 179cm wearing Size S",
+        "The garment’s color may differ slightly when it arrives from the Tailory.",
+        ],
+
+        swatches: [
+        {color: "#232325", label: "Onyx Black"},
+        {color: "#232325", label: "Onyx Black"},
+        {color: "#232325", label: "Onyx Black"},
+        {color: "#232325", label: "Onyx Black"}
+
+        ];
+
+        
+    
+
+
+    },
+
+
+
+
+
+
+
+
+
+
+
+},
+
+
+
+
+
+
+];
